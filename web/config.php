3cddd52468d9d7a20f8e917adbef276b66406be0
Janusz Go <godlewskijanek@gmail.com>
2025-09-17 13:18:47 +0200

    fixed path of sqlite db

--- web/config.php
+++ web/config.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 try {
-    $pdo = new PDO('sqlite:' . __DIR__ . '\database.sqlite');
+    $pdo = new PDO('sqlite:' . __DIR__ . '/database.sqlite');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Połączenie nieudane: " . $e->getMessage());
